7518318f3e9ec9c912010ee57bb6e11e915640bd
Althaf NS <althafns1441@gmail.com>
2026-04-08 15:36:57 +0530

    refactored code using functions and parameters

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 list_expense  = []
 
+def add_expenses(expenses):
+    expense = int(input("Enter the expense: "))
+    expenses.append(expense)
+
+def view_expenses(expenses):
+    print("list of expense",expenses)
+    print("total expenses: ",sum(expenses))
+
 while(True):
     print("1. Add Expense")
     print("2. View Expenses")
@@ -8,11 +16,9 @@ while(True):
     choose = int(input("Enter the corresponding number to choose: "))
 
     if(choose == 1):
-        expense = int(input("Enter the expense"))
-        list_expense.append(expense)
+        add_expenses(list_expense)
     elif(choose == 2):
-        print("list of expense",list_expense)
-        print("total expenses: ",sum(list_expense))
+        view_expenses(list_expense)
     elif(choose == 3):
         print("Exiting...")
         break
